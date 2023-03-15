3530a3c3b85e2dcf980e5fc4ba62d6862dbac415
Muhammad Usman <muhammad.usman@theowlsense.com>
2023-03-15 21:06:37 +0500

    Added Signup and Contact Form Functionality

--- pkinternational/admin.py
+++ pkinternational/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Car, CarImage, CarColor, CarModel, FrequentQuestion, FrequesntAnswers
-
+from django.contrib.auth.models import User
+from .models import ContactModel
 
 class CarImageInlineModels(admin.TabularInline):
     model = CarImage
@@ -25,3 +26,4 @@ class AnsweredInlineModel(admin.ModelAdmin):
 admin.site.register(Car, CarInlineModel)
 admin.site.register(CarModel)
 admin.site.register(FrequentQuestion, AnsweredInlineModel)
+admin.site.register(ContactModel)
--- pkinternational/views.py
+++ pkinternational/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.translation import gettext as _
 from django.utils.translation import gettext, get_language, activate
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import login as userLogin
+
+from .models import ContactModel
 
 
 def home(request):
@@ -13,15 +19,45 @@ def our_team(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        name = request.POST.get("name")
+        msg = request.POST.get("msg")
+        if email and name:
+            contact = ContactModel(email=email, name=name, msg=msg)
+            contact.save()
+            messages.info(request, f"Thanks for reaching us we will contact you soon")
     return render(request, "pages/contact.html")
 
 
 def login(request):
+    if request.method == "POST":
+        print("[+] Login Post Form ")
+        username = request.POST.get('username')
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            userLogin(request, user)
+            messages.info(request, f"You are now logged in as {username}.")
+            return redirect("home")
+        else:
+            messages.error(request, f"Account Does not exist's please sign up")
     return render(request, "pages/login.html")
 
 
 def register(request):
+    if request.method == "POST":
+        print(f"[+] The data recived is {request.POST['username']}")
+        username = request.POST.get('username')
+        email = request.POST['email']
+        password = request.POST['password']
+        user = User(email=email, username=username, password=password)
+        user.save()
+        messages.info(request, f"Sign Up Complete")
+        return redirect('login')
+
     return render(request, "pages/register.html")
+
 
 def search(request):
     return render(request, "pages/search.html")
