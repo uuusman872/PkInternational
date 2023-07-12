67e6e196c7a0de139872db96c0c5d99e19c01b1b
usman-software <muhammad.usman@theowlsense.com>
2023-07-12 20:14:39 +0500

    url_pattern updated

--- pkinternational/urls.py
+++ pkinternational/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from django.conf.urls.i18n import i18n_patterns
+
 admin.site.site_header = "株式会社 PK INTERNATIONAL Admin"
 admin.site.site_title = "株式会社 PK INTERNATIONAL Admin Portal"
 admin.site.index_title = "Welcome to 株式会社 PK INTERNATIONAL Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rosetta/', include('rosetta.urls')),
     path("", views.home, name="home"),
     path("contact", views.contact, name="contact"),
     path("team", views.our_team, name="team"),
@@ -16,7 +18,8 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("search", views.search, name="search"),
     path("sell", views.sell, name="sell"),
-    path("about", views.about, name="about")
+    path("about", views.about, name="about"),
+    path("cars_list_view", views.cars_list_view, name="cars_list_view")
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
